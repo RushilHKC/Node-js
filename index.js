deeca69a785335b0dde6f44b2ad8a1c5b239dda0
Harshwardhan <harsh28123@gmail.com>
2025-09-28 02:04:45 +0530

    blog card creation with static image rendering

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const userDB = require("./models/user");
+const blogDB = require("./models/blog.js");
 const {connectDB} = require("./connection.js");
 const userRoute = require("./routes/user.js");
 const blogRouter = require("./routes/blog.js");
 const {verifyToken} = require("./Auth/auth.js")
 const parser = require('cookie-parser');
+
+
 
 try{
     connectDB("mongodb://127.0.0.1:27017/blogify").then(()=> console.log("Database Connected Successfully"));
@@ -16,15 +19,19 @@ catch{
 }
 
 app.use(express.urlencoded({extended: true}));
-app.use(parser())
+app.use(parser());
+app.use(express.static('public'));
 
 app.set("view engine","ejs");
 app.set("views",path.resolve("./views"));
 app.use("/user",userRoute);
 app.use("/addBlog",blogRouter);
 
-app.get("/",verifyToken,(req,res)=>{
-    return res.render("home");
+app.get("/",verifyToken, async(req,res)=>{
+    bgs = await blogDB.find({});
+    return res.render("home",{
+        blogs:bgs
+    });
 });
 
 app.listen(8000,()=> console.log("Server Started"));    
